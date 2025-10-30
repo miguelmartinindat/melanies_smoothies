969558eabb85a33777526d27b2a7c687095086e5
miguelmartinindat <57622316+miguelmartinindat@users.noreply.github.com>
2025-10-30 08:53:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,9 @@ st.write("The current movie title is", title)
 
 
 ##session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 ##st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect ('Choose up to 5 ingredients:', my_dataframe,max_selections=5)
 
